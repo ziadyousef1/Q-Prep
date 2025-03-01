3240fedd088c85feb2c440900e2851ede2318442
Reda Saad <Reda.Saad@gmail.com>
2025-03-01 22:22:04 +0200

    Add Docs

--- ProjectAPI/Program.cs
+++ ProjectAPI/Program.cs
@@ -108,12 +108,10 @@ namespace ProjectAPI
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+          
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
             app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
