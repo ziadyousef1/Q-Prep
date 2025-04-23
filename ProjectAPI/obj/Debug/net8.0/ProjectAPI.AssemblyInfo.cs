1fe9c5207d5303715210cc4214fa087715882110
Reda Saad <Reda.Saad@gmail.com>
2025-04-23 17:49:22 +0200

    commit23

--- ProjectAPI/obj/Debug/net8.0/ProjectAPI.AssemblyInfo.cs
+++ ProjectAPI/obj/Debug/net8.0/ProjectAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94995cbedec1a22a7076f65b25f6ed548f29df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae729a6da6e42c98af4314c8817f5687283d7c59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
